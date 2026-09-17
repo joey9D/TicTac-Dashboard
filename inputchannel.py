145f010f4b5cceda62bbc7b00db6995f710a7ebc
Jan_work <jan.kristel@ws-schaefer.com>
2026-09-17 14:23:54 +0200

    add label update from input

--- inputchannel.py
+++ inputchannel.py
@@ -30,6 +30,9 @@ class InputChannel(QtCore.QObject):
         # Notify Qt
         label = getattr(state.window, f"l_CntInVal{self.number}")
         label.setText(str(self.counter))
+        label_led = getattr(state.window, f"lbl_InputCnt{self.number}")
+        label_led.setText(str(self.counter))
+        
 
     def update(self):
         # Nothing required here.
